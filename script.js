113ae2bb7a240403e5b5435acac9fded7eb452f1
AoiSejima <sejia949@newschool.edu>
2022-01-14 22:12:40 -0500

    Updated Works Page

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
-// Toggle Hide and Show
-let toggleWorkTOC = document.getElementById("toggle-work-TOC");
-// let toggleOtherWorks = document.getElementById("toggle-otherWorks");
+const showContent = function(event) {
+    const clickedLink = event.target;
+    if (!clickedLink || !clickedLink.classList.contains("href")) {
+        return;
+    }
+
+    document.querySelectorAll("a.show.active").forEach(a => a.classList.remove("active"));
+    clickedLink.classList.add("active");
+
+    const targetContent = document.querySelector(clickedLink.getAttribute("href"));
+    document.querySelectorAll(".content").forEach(el => el.style.display = "none");
+    targetContent.style.display = "grid";
+
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+	
+	event.preventDefault();
+};
+
+document.querySelector("nav.nav").addEventListener("click", showContent);
+document.querySelectorAll(".content:not(:first-child)").forEach(el => el.style.display = "none");
+
+
+
+
 
 let toggleAbout = document.getElementById("toggle-about");
 let toggleContact = document.getElementById("toggle-contact");
-
-
-function buttonWorkTOC() {
-    if (toggleWorkTOC.style.display === "block") {
-        toggleWorkTOC.style.display = "none";
-    } else {
-        toggleWorkTOC.style.display = "block";
-        toggleAbout.style.display = "none";
-        toggleContact.style.display = "none";
-    }
-};
-
-// function buttonOtherWorks() {
-//     if (toggleOtherWorks.style.display === "block") {
-//         toggleOtherWorks.style.display = "none";
-//     } else {
-//         toggleOtherWorks.style.display = "block";
-//         toggleAbout.style.display = "none";
-//         toggleContact.style.display = "none";
-//     }
-// };
 
 function buttonAbout() {
     if (toggleAbout.style.display === "block") {
         toggleAbout.style.display = "none";
     } else {
         toggleAbout.style.display = "block";
-        toggleWorkTOC.style.display = "none";
         toggleContact.style.display = "none";
     }
 };
@@ -41,37 +44,12 @@ function buttonContact() {
         toggleContact.style.display = "none";
     } else {
         toggleContact.style.display = "block";
-        toggleWorkTOC.style.display = "none";
         toggleAbout.style.display = "none";
     }
 };
 
-let buttonTabWebsite = document.getElementById("buttonTabWebsite");
-function buttonActivateTabWebsite() {
-    if (buttonTabWebsite.style.display === "block") {
-        buttonTabWebsite.style.display = "none";
-    } else {
-        buttonTabWebsite.style.display = "block";
-        // toggleWorkTOC.style.display = "none";
-        // toggleAbout.style.display = "none";
-    }
-};
 
-
-
-let collapsibleTabs = document.getElementsByClassName("collapsibleTabs");
-let i;
-
-for (i = 0; i < collapsibleTabs.length; i++) {
-  collapsibleTabs[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-
-    let content = this.nextElementSibling;
-
-    if (content.style.display === "block") {
-        content.style.display = "none";
-    } else {
-        content.style.display = "block";
-    }
-  });
+function myFunction(selectTag) {
+    let listValue = selectTag.options[selectTag.selectedIndex].text;
+    document.getElementById("nav-li").style.fontFamily = listValue;
 }
