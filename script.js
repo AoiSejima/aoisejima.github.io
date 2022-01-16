410a056f39a4d9b9ee30a280b55ed37cf821c110
AoiSejima <sejia949@newschool.edu>
2022-01-16 03:28:18 -0500

    Updated  Home Page Grid, Added description hover effect

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .navContainer ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
+
+
 const showContent = function(event) {
     const clickedLink = event.target;
-    if (!clickedLink || !clickedLink.classList.contains("href")) {
+    if (!clickedLink || !clickedLink.classList.contains("show")) {
         return;
     }
 
@@ -11,12 +31,6 @@ const showContent = function(event) {
     document.querySelectorAll(".content").forEach(el => el.style.display = "none");
     targetContent.style.display = "grid";
 
-    window.scroll({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    });
-	
 	event.preventDefault();
 };
 
@@ -27,29 +41,30 @@ document.querySelectorAll(".content:not(:first-child)").forEach(el => el.style.d
 
 
 
-let toggleAbout = document.getElementById("toggle-about");
-let toggleContact = document.getElementById("toggle-contact");
 
-function buttonAbout() {
-    if (toggleAbout.style.display === "block") {
-        toggleAbout.style.display = "none";
-    } else {
-        toggleAbout.style.display = "block";
-        toggleContact.style.display = "none";
-    }
-};
+// let toggleAbout = document.getElementById("toggle-about");
+// let toggleContact = document.getElementById("toggle-contact");
 
-function buttonContact() {
-    if (toggleContact.style.display === "block") {
-        toggleContact.style.display = "none";
-    } else {
-        toggleContact.style.display = "block";
-        toggleAbout.style.display = "none";
-    }
-};
+// function buttonAbout() {
+//     if (toggleAbout.style.display === "block") {
+//         toggleAbout.style.display = "none";
+//     } else {
+//         toggleAbout.style.display = "block";
+//         toggleContact.style.display = "none";
+//     }
+// };
+
+// function buttonContact() {
+//     if (toggleContact.style.display === "block") {
+//         toggleContact.style.display = "none";
+//     } else {
+//         toggleContact.style.display = "block";
+//         toggleAbout.style.display = "none";
+//     }
+// };
 
 
-function myFunction(selectTag) {
-    let listValue = selectTag.options[selectTag.selectedIndex].text;
-    document.getElementById("nav-li").style.fontFamily = listValue;
-}
+// function myFunction(selectTag) {
+//     let listValue = selectTag.options[selectTag.selectedIndex].text;
+//     document.getElementById("nav-li").style.fontFamily = listValue;
+// }
